27af18abfe60990410a10ade1b61b8b6350913d9
Julian Grinblat <julian@dotcore.co.il>
2021-10-25 23:32:15 +0900

    Do not dirty repo when installing via npm (#119)

--- lib/install.sh
+++ lib/install.sh
@@ -9,7 +9,7 @@ if [ -x "$(command -v yarn)" ]; then
   ln -fs "${LIBDIR}/node_modules/.bin/lehre" "${LIBDIR}/lehre"
 elif [ -x "$(command -v npm)" ]; then
   cd "$LIBDIR"
-  npm install
+  npm install --no-package-lock
   ln -fs "${LIBDIR}/node_modules/.bin/lehre" "${LIBDIR}/lehre"
 else
   echo 'Neither yarn nor npm was found on your path' >&2
